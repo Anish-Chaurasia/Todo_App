737f0d06a4e7ccefe61b289a47690057812c14fd
Anish Chaurasia <anishraut2111@gmail.com>
2023-05-16 01:09:38 +0530

    Todo_App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [initialval, setInitialval] = useState("");
+    const [listval, setListval] = useState([]);
+
+    function addTask() {
+
+        setListval((listval) => {
+            const updated = [...listval, initialval];
+            setInitialval("")
+            return updated;
+        })
+    }
+
+    function removeTask(x) {
+        const newarry = listval.filter((val, id) => { return x != id; });
+        setListval(newarry);
+
+    }
+
+    function clearall() {
+        setListval([]);
+    }
+
+
+    return (
+        <>
+            <div className="external">
+                <div className="internal">
+                    <h1>TODO </h1>
+
+
+                    <input type="text" placeholder="Add New Task" className="taskinput" value={initialval} onChange={(e) => setInitialval(e.target.value)} />
+                    <button type="button" className="addbutton" onClick={addTask}> Add </button>
+                    <button type="button" className="clearbutton" onClick={clearall} >Reset</button>
+
+
+
+
+                    {
+                        listval.map((item, index) => {
+                            return (<>
+                                <p className="list" key={index}>{item}  <button type="button" className="removebutton" onClick={() => removeTask(index)}>Remove</button></p>
+
+                            </>)
+
+
+                        })
+                    }
+
+
+
+                </div>
+
+            </div>
+            <p className="name">&#9827; anish_chaurasia</p>
+
+
+        </>
+
+
+
+    );
+
 }
 
+
 export default App;
+
